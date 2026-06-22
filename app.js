7201abdca5a0c3fa65e68d2ee5ba51dab7cf6c0d
Kevin <thevoidkc@gmail.com>
2026-06-22 15:12:55 -0700

    feat: style the ui

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const db = require("./db/queries");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // index
 app.get("/", async (req, res) => {
